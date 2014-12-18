9a92bf86dcdd3602c58b41ae641c5df2823f5a1a
Yasam Bingol <yasam.bingol@gmail.com>
2014-12-18 12:59:24 +0200

    eclipse kernel project

---
+++ eclipse/eprojects_kernel.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+
+if [ $# == 1 ];then
+    echo "Changing directory : $1"
+    cd $1
+fi
+
+PROJECT_DIR=`pwd`
+
+TEMPLATE_DIR=/home/yasam/projects/tools/eclipse
+
+NAME=`basename $PROJECT_DIR`
+
+echo "Creating project on $PROJECT_DIR"
+echo "Project name : $NAME"
+
+echo "Coping project files"
+
+cp $TEMPLATE_DIR/.project ./
+cp $TEMPLATE_DIR/.cproject ./
+
+echo "Replacing project name with : $NAME"
+cat .project | sed 's/@@__NAME__@@/'$NAME'/g' > .tmpproject
+mv .tmpproject .project
+
+
+cp $TEMPLATE_DIR/.project_kernel ./.project
+cp $TEMPLATE_DIR/.cproject_kernel ./.kernel
+
+echo "Replacing project name with : $NAME"
+cat .project | sed 's/@@__NAME__@@/'$NAME'/g' > .tmpproject
+mv .tmpproject .project
+
+
